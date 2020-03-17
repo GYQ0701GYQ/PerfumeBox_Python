1f97d7704f34be6fef3c10af6bd26a76331a81a7
GYQ0701GYQ <GYQ0701GYQ@163.com>
2020-03-17 17:23:34 +0800

    在Test.py注释部分是python测试代码，未注释部分是用py2neo做的neo4j数据库连接测试，查询了单个节点并返回。

---
+++ PerfumeBoxProject/PerfumeBox/scripts/Test.py
@@ -0,0 +1,36 @@
+# A first Python script
+'''
+import sys                  # Load a library module
+print(sys.platform)
+print(2 ** 100)             # Raise 2 to a power
+x = 'Spam!'
+print(x * 8)                # String repetition
+
+import django
+import os
+import sys
+
+# 这两行很重要，用来寻找项目根目录，os.path.dirname要写多少个根据要运行的python文件到根目录的层数决定
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(BASE_DIR)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PerfumeBoxProject.settings')
+django.setup()
+
+# !/usr/bin/python3
+print("Hello,A first Python script!")
+'''
+import py2neo
+from py2neo import Graph, Node, Relationship
+print(py2neo.__version__)
+graph = Graph(
+    "http://localhost:7474",
+    username="neo4j",
+    password="987qazwsxedc"
+)
+find_code_1 = graph.find_one(
+  label="Person",
+  property_key="name",
+  property_value="Joel Silver"
+)
+print(find_code_1['born'])
