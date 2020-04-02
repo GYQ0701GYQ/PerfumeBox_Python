664588567d0d5832057b81c8f91dbee7b2a8e80a
GYQ0701GYQ <GYQ0701GYQ@163.com>
2020-04-02 14:22:59 +0800

    封装了axios，用axios请求后端，目前在主页输入数据可以返回到页面上

--- PerfumeBoxProject/PerfumeBox/views.py
+++ PerfumeBoxProject/PerfumeBox/views.py
@@ -1,3 +1,4 @@
+import re
 from django.shortcuts import render
 
 # Create your views here.
@@ -17,15 +18,31 @@ res = []
 @require_http_methods(['GET'])
 def search_perfume(request):
     response = {}
+    res1 = []
+    res2 = []
     try:
         print('触发了search_perfume')
-        # find_code_1 = graph.find_one(
-        #     label="Person",
-        #     property_key="name",
-        #     property_value="Carrie-Anne Moss"
-        # )
-        # print(find_code_1)
-        print(request)
+        search_name = request.GET.get('search_info')
+        name_list = []
+        g = graph.run('MATCH (p:Perfume) return p.perfume_name').data()
+        for item in g:
+            if re.search(search_name, item['p.perfume_name']) is not None:
+                # one_name = re.search(search_name, item['p.perfume_name']).group()
+                name_list.append(item['p.perfume_name'])
+        print(name_list)
+        for each_name in name_list:
+            g = graph.run('MATCH (a:Perfume{perfume_name:"'+str(each_name)+'"}) RETURN a').data()
+            temp_dict = dict(g[0]['a'])
+            res1_k = []
+            res2_k = []
+            for k in temp_dict:
+                print(k, temp_dict[k])
+                res1_k.append(k)
+                res2_k.append(temp_dict[k])
+            res1.append(res1_k)
+            res2.append(res2_k)
+        response['list1'] = res1
+        response['list2'] = res2
         response['msg'] = 'success'
         response['error_num'] = 0
         print('search_perfume结束')
