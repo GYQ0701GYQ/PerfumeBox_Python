bdfe25931c0aa8744919b939ca20a840f9b8e373
GYQ0701GYQ <GYQ0701GYQ@163.com>
2020-04-03 22:13:12 +0800

    将perfume_detail页面的搜索结果格式化后返回前端

--- PerfumeBoxProject/PerfumeBox/views.py
+++ PerfumeBoxProject/PerfumeBox/views.py
@@ -10,7 +10,6 @@ from django.http import JsonResponse
 import json
 from py2neo import Graph, Node, Relationship
 
-
 graph = Graph("http://localhost:7474", username="neo4j", password="987qazwsxedc")
 res = []
 
@@ -18,7 +17,6 @@ res = []
 @require_http_methods(['GET'])
 def search_perfume(request):
     response = {}
-    res1 = []
     res2 = []
     try:
         print('触发了search_perfume')
@@ -31,18 +29,37 @@ def search_perfume(request):
                 name_list.append(item['p.perfume_name'])
         print(name_list)
         for each_name in name_list:
-            g = graph.run('MATCH (a:Perfume{perfume_name:"'+str(each_name)+'"}) RETURN a').data()
-            temp_dict = dict(g[0]['a'])
-            res1_k = []
-            res2_k = []
+            g = graph.run('MATCH (a:Perfume{perfume_name:"' + str(each_name) + '"}) RETURN a').data()
+            temp_dict = dict(g[0]['a'])  # 返回符合查询条件的所有香水
+            print(temp_dict)
+            res1 = ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#']
             for k in temp_dict:
-                print(k, temp_dict[k])
-                res1_k.append(k)
-                res2_k.append(temp_dict[k])
-            res1.append(res1_k)
-            res2.append(res2_k)
-        response['list1'] = res1
-        response['list2'] = res2
+                if k == 'perfume_tag':
+                    res1[0] = temp_dict[k]
+                elif k == 'perfume_published':
+                    res1[1] = temp_dict[k]
+                elif k == 'perfume_name':
+                    res1[2] = temp_dict[k]
+                elif k == 'perfume_img':
+                    res1[3] = temp_dict[k]
+                elif k == 'perfume_fragment':
+                    res1[4] = temp_dict[k]
+                elif k == 'perfume_attribute':
+                    res1[5] = temp_dict[k]
+                elif k == 'perfume_brief':
+                    res1[6] = temp_dict[k]
+                elif k == 'perfume_brand':
+                    res1[7] = temp_dict[k]
+                elif k == 'second_fragment':
+                    res1[8] = temp_dict[k]
+                elif k == 'first_fragment':
+                    res1[9] = temp_dict[k]
+                elif k == 'perfume_flavorist':
+                    res1[10] = temp_dict[k]
+                elif k == 'third_fragment':
+                    res1[11] = temp_dict[k]
+            res2.append(res1)
+        response['list'] = res2
         response['msg'] = 'success'
         response['error_num'] = 0
         print('search_perfume结束')
@@ -73,4 +90,3 @@ def show_perfume(request):
         response['error_num'] = 1
 
     return JsonResponse(response)
-
